ab07668eda938f4f11c40235265dee2ba0d1b4ac
9260102080 <82657940+9260102080@users.noreply.github.com>
2021-06-22 13:36:58 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -2,9 +2,24 @@ package exam;
 
 public class CountCombinations {
 
-  public int getNumberOfWays(final int total, final int k) {
+  public int getNumberOfWays(final int total, final int ) {
+    public static void main(string[]args)
+    {
+      System.out.println("Printing Pattern");
+    }
+      for(i=0;i<n;i++)
+      {
+        for(i=0;i<n;j++)
+        {
+          System.out.println("%d",i);
+        }
+          for(j=0;i<n;i-+)
+          {
+        for(j=0;i<n;i--)
+          System.out.println("%d",i);
+        }   
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
     return 0;
   }
 
-}
+
