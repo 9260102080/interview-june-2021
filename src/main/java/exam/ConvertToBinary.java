c67fc79ba6fe97a0df35f88d3480d29e01d44ba6
9260102080 <82657940+9260102080@users.noreply.github.com>
2021-06-22 13:44:34 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,29 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
+    int n=1;
+for(int i=0;i<n;i++)
+{
+int j,last;
+last=arr[arr.length-1];
+for(j=arr.length-1;j>0;j--)
+{
+arr[j]=arr[j-1];
+}
+arr[0]=last;
+}
+for(int i=0;i<arr.length;i++)
+System.out.print(arr[i]+" ");
+return 0;
+    }
+public static void main(String[] args)
+{
+int n = 17;
+decToBinary(n);
+}
+}
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    
     return "";
   }
 }
